ce86b9f5b5c4c3a02c079caceac1c08e58db6b95
Mohammad Javad Davoudabadi <mohammad.davoudabadi@sydney.edu.au>
2025-03-18 10:11:12 +1100

    Modified the name and class of functions

--- src/lasso_distribution.h
+++ src/lasso_distribution.h
@@ -10,20 +10,20 @@ double expit_c(double x);
 //Rcpp::List calculate_lasso_dist_stats_c(double a_val, double b_val, double c_val);
 //double logSumExp_c(vec vx);
 double zlasso_c_v1(double a_val, double b_val, double c_val, bool logarithm);
-double zlasso_c_v2(double a_val, double b_val, double c_val, bool logarithm);
+double zlasso(double a_val, double b_val, double c_val, bool logarithm);
 arma::vec dlasso_c_v1(arma::vec vx, double a_val, double b_val, double c_val, bool logarithm);
-arma::vec dlasso_c_v2(arma::vec vx, double a_val, double b_val, double c_val, bool logarithm);
+arma::vec dlasso(arma::vec vx, double a_val, double b_val, double c_val, bool logarithm);
 arma::vec plasso_c_v1(arma::vec vx, double a_val, double b_val, double c_val);
-arma::vec plasso_c_v2(arma::vec vx, double a_val, double b_val, double c_val);
-double qlasso_fast_c_v1(double u, double a_val, double b_val, double c_val);
-double qlasso_fast_c_v2(double u, double a_val, double b_val, double c_val);
-double rlasso_fast_c_v1(double a_val, double b_val, double c_val);
-double rlasso_fast_c_v2(double a_val, double b_val, double c_val);
+arma::vec plasso(arma::vec vx, double a_val, double b_val, double c_val);
+arma::vec qlasso_fast_c_v1(arma::vec u, double a_val, double b_val, double c_val);
+arma::vec qlasso(arma::vec u, double a_val, double b_val, double c_val);
+arma::vec rlasso_fast_c_v1(double n, double a_val, double b_val, double c_val);
+arma::vec rlasso(double n, double a_val, double b_val, double c_val);
 double elasso_c_v1(double a_val, double b_val, double c_val);
-double elasso_c_v2(double a_val, double b_val, double c_val);
+double elasso(double a_val, double b_val, double c_val);
 double vlasso_c_v1(double a_val, double b_val, double c_val);
-double vlasso_c_v2(double a_val, double b_val, double c_val);
-double mlasso_c(double a_val, double b_val, double c_val);
+double vlasso(double a_val, double b_val, double c_val);
+arma::vec mlasso(arma::vec a_val, arma::vec b_val, arma::vec c_val);
 
 ////////////////////////////////////////////////////////////////////////////////
 
