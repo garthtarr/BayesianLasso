3a3b58238e3f35980c8fb4612b905b4b6d54501a
Mohammad Javad Davoudabadi <mohammad.davoudabadi@sydney.edu.au>
2026-04-08 11:27:19 +1000

    reproduction files modified

--- reproduction/reproduce_all.R
+++ reproduction/reproduce_all.R
@@ -11,12 +11,7 @@
 # reproduce_all.R
 
 # install/load BayesianLasso from GitHub master branch
-if (!requireNamespace("BayesianLasso", quietly = TRUE)) {
-  if (!requireNamespace("remotes", quietly = TRUE)) {
-    install.packages("remotes")
-  }
-  remotes::install_github("garthtarr/BayesianLasso", ref = "master", upgrade = "never")
-}
+
 
 library(BayesianLasso)
 
@@ -27,37 +22,37 @@ setwd(repro_dir)
 results_dir <- "results"
 if (!dir.exists(results_dir)) dir.create(results_dir, recursive = TRUE)
 
-datasets_ngtp <- c("simulated", "diabetes2", "Kakadu2", "Crime")
+# datasets_ngtp <- c("simulated", "diabetes2", "Kakadu2", "Crime")
 
-time_val_total_ngtp <- system.time({
-  for (d in datasets_ngtp) {
-    dataset_name <- d
-    source("Benchmarks_ngtp.R", local = FALSE)
-  }
-})[3]
+# time_val_total_ngtp <- system.time({
+#   for (d in datasets_ngtp) {
+#     dataset_name <- d
+#     source("Benchmarks_ngtp.R", local = FALSE)
+#   }
+# })[3]
+# 
+# print(time_val_total_ngtp)
 
-print(time_val_total_ngtp)
+datasets_pgtn <- c("eyedata") # "cookie",
 
-# datasets_pgtn <- c("cookie", "eyedata")
-# 
-# 
-# 
-# for (d in datasets_pgtn) {
-#   print(d)
-#   rmarkdown::render(
-#     "benchmarks_pgtn.Rmd",
-#     params = list(dataset_name = d, results_dir = "results"),
-#     envir = new.env(parent = globalenv())
-#   )
-# }
-# 
-# 
-# load(file.path(results_dir,"eyedata_results_pgtn.Rdata"))
-# 
-# print(colMeans(res_PC$mStat))
-# print(colMeans(res_hans$mStat))
-# print(colMeans(res_bayesreg$mStat))
-# print(time_val_total_pgtn)
+
+
+for (d in datasets_pgtn) {
+  print(d)
+  rmarkdown::render(
+    "benchmarks_pgtn.Rmd",
+    params = list(dataset_name = d, results_dir = "results"),
+    envir = new.env(parent = globalenv())
+  )
+}
+
+
+load(file.path(results_dir,"eyedata_results_pgtn.Rdata"))
+
+print(colMeans(res_PC$mStat))
+print(colMeans(res_hans$mStat))
+print(colMeans(res_bayesreg$mStat))
+print(time_val_total_pgtn)
 # 
 # 
 
